af9111d69261e90b638e472408783d61a60f7b9c
mulimuoki001 <b.muoki@alustudent.com>
2023-07-10 00:10:14 +0200

    0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,28 +1,47 @@
 #!/usr/bin/python3
-"""Module"""
 
 import requests
 import sys
 
-if __name__ == "__main__":
-    employee_id = sys.argv[1]
-    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
-    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos/".format(
-        employee_id
+# Check if the employee ID is provided as a command line argument
+if len(sys.argv) < 2:
+    print("Please provide an employee ID as a command line argument")
+    sys.exit(1)
+
+employee_id = sys.argv[1]
+
+# Make a GET request to the API endpoint to retrieve employee details
+employee_response = requests.get(
+    f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+)
+
+# Check if the request was successful (status code 200)
+if employee_response.status_code == 200:
+    employee_data = employee_response.json()  # Convert the response to JSON
+
+    # Fetch the employee name
+    emp_name = employee_data["name"]
+
+    # Make a GET request to retrieve the TODO list for the employee
+    todos_response = requests.get(
+        f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
     )
 
-    user_info = requests.get(user_url).json()
-    todos_info = requests.get(todos_url).json()
+    # Check if the request was successful (status code 200)
+    if todos_response.status_code == 200:
+        todos = todos_response.json()  # Convert the response to JSON
 
-    employee_name = user_info["name"]
-    task_completed = list(filter(lambda obj: (obj["completed"] is True), todos_info))
-    number_of_done_tasks = len(task_completed)
-    total_number_of_tasks = len(todos_info)
+        # Filter the completed tasks for the employee
+        comp_tasks = [todo["title"] for todo in todos if todo["completed"]]
 
-    print(
-        "Employee {} is done with tasks({}/{}):".format(
-            employee_name, number_of_done_tasks, total_number_of_tasks
+        # Display the employee TODO list progress
+        print(
+            f"Employee {emp_name} is done with tasks"
+            f"({len(comp_tasks)}/{len(todos)}):"
         )
-    )
-
-    [print("\t " + task["title"]) for task in task_completed]
+        for task in comp_tasks:
+            print(f"    {task}")
+    else:
+        print(f"Error: Failed  for employee" f" {employee_id}")
+else:
+    print(f"Error: Failed  for employee" f"{employee_id}")
